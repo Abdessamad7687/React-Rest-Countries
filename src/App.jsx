0e580180d75aa247810af392a41acabd2c8b77f9
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-03-04 14:48:16 +0100

    RestCountries

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Divider, Stack } from '@mui/material'
-import RecipeReviewCard from './components/RecipeReviewCard'
+import DataTable from './components/DataTable'
 
 const App = () => {
+  const [countries, setCountries] = useState([])
   const fetchData = async () => {
     const response = await fetch('https://restcountries.com/v3.1/all')
     const data = await response.json()
-    console.log(data)
+    setCountries(data)
   }
   useEffect(() => {
     fetchData()
   }, [])
-  
+
   return (
     <Stack direction="row"
       justifyContent="center"
       alignItems="center"
-      sx={{flexWrap: 'wrap', margin: '50px'}}
+      sx={{ flexWrap: 'wrap', margin: '50px' }}
       divider={<Divider orientation="vertical" flexItem />}
       spacing={2}>
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
-      <RecipeReviewCard />
+
+      {/* {
+        countries.map((country, index) => (
+          <DataTable
+            sx={{ marginTop: '20px' }}
+            key={index}
+            
+          />
+        ))
+      } */}
+      <DataTable           
+          />
     </Stack>
 
   )
